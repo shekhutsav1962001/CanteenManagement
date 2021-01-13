e5ab062fb527ad89fa7401090af7ac7025cf058c
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-01-13 13:08:57 +0530

    change in user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,12 +3,12 @@ var router = express.Router()
 const useController = require('../controller/userController')
 require('dotenv').config()
 
-router.get('/abc', (req, res) => {
-    console.log(process.env.ABC)
-    console.log(process.env.PQR)
-    console.log(process.env.SECRETKEY);
-    res.send("hello get abc");
-})
+// router.get('/abc', (req, res) => {
+//     console.log(process.env.ABC)
+//     console.log(process.env.PQR)
+//     console.log(process.env.SECRETKEY);
+//     res.send("hello get abc");
+// })
 
 
 
