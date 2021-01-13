ff8be791cf41b766f13518d74453cdaa907970aa
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-01-13 23:25:36 +0530

    quantity validation added

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -5,7 +5,7 @@ exports.addFood = (req, res) => {
     var file = req.file
     var food = new Food({
         foodname: req.body.foodname,
-        foodqty : req.body.foodqty,
+        foodqty: req.body.foodqty,
         foodprice: req.body.foodprice,
         foodimage: file.filename
     })
@@ -15,6 +15,7 @@ exports.addFood = (req, res) => {
         return res.json({ msg: 'Food added' });
     }
     catch (err) {
+        console.log("some error while adding food by admin")
         return res.json({ errormsg: 'Somthing went wrong' });
     }
 
@@ -33,42 +34,85 @@ exports.getallFoodItem = (req, res) => {
 
 
 exports.editFood = (req, res) => {
-    console.log(req.body);
-    Food.updateOne({ _id: req.body._id }, {
-        foodname: req.body.foodname,
-        foodprice: req.body.foodprice,
-        foodqty : req.body.foodqty
-    }, function (err, items) {
+    let avail;
+    if (req.body.foodqty) {
+        if (!isNaN(req.body.foodqty)) {
+            if (req.body.foodqty <= 0) {
+                avail = false;
+            }
+            else {
+                avail = true;
+            }
+            Food.updateOne({ _id: req.body._id }, {
+                foodname: req.body.foodname,
+                foodprice: req.body.foodprice,
+                foodqty: req.body.foodqty,
+                foodavail: avail
+            }, function (err, item) {
 
-        if (err) {
-            console.log("some error")
-            return res.json({ errormsg: 'Somthing went wrong' });
+                if (err) {
+                    console.log("some error in edit food without image")
+                    return res.json({ errormsg: 'Somthing went wrong' });
+                }
+                else {
+                    console.log("Edited food without image");
+                    return res.json({ msg: 'Edited food without image' });
+                }
+            })
         }
         else {
-            console.log("Edited food without image");
-            return res.json({msg: 'Edited food without image' });
+            console.log("Invalid Quantity!");
+            return res.json({ errormsg: 'Invalid Quantity!' });
         }
-    })
+    }
+    else {
+        console.log("Invalid Quantity!");
+        return res.json({ errormsg: 'Invalid Quantity!' });
+    }
+
+
 }
 
 exports.editFoodWithImage = (req, res) => {
-    var file = req.file
-    Food.updateOne({ _id: req.body._id }, {
-        foodname: req.body.foodname,
-        foodprice: req.body.foodprice,
-        foodqty : req.body.foodqty,
-        foodimage: file.filename
-
-    }, function (err, items) {
-        if (err) {
-            console.log("some error")
-            return res.json({ errormsg: 'Somthing went wrong' });
+    let avail;
+    if (req.body.foodqty) {
+        if (!isNaN(req.body.foodqty)) {
+            if (req.body.foodqty <= 0) {
+                avail = false;
+            }
+            else {
+                avail = true;
+            }
+            var file = req.file
+            console.log(req.body.foodqty);
+            Food.updateOne({ _id: req.body._id }, {
+                foodname: req.body.foodname,
+                foodprice: req.body.foodprice,
+                foodqty: req.body.foodqty,
+                foodimage: file.filename,
+                foodavail: avail
+            }, function (err, item) {
+                if (err) {
+                    console.log("some error in edit food with image")
+                    return res.json({ errormsg: 'Somthing went wrong' });
+                }
+                else {
+                    console.log("Edited food with image");
+                    return res.json({ msg: 'Edited food with image' });
+                }
+            })
         }
         else {
-            console.log("Edited food with image");
-            return res.json({msg: 'Edited food with image' });
+            console.log("Invalid Quantity!");
+            return res.json({ errormsg: 'Invalid Quantity!' });
         }
-    })
+    }
+    else {
+        console.log("Invalid Quantity!");
+        return res.json({ errormsg: 'Invalid Quantity!' });
+    }
+
+
 }
 
 exports.deleteFood = (req, res) => {
