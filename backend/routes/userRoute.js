b68cf9293b88910d1f24ae8def5aa28ec8e3b618
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-02-16 19:47:33 +0530

    bug solved

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -14,6 +14,5 @@ router.get('/getcart',verifyTokenmiddleware.verifyToken,useController.getCart)
 router.post('/deletefromcart', verifyTokenmiddleware.verifyToken,useController.deleteFromCart)
 router.post('/paytm', verifyTokenmiddleware.verifyToken,useController.paytm)
 router.post('/placeorder', verifyTokenmiddleware.verifyToken,useController.placeOrder)
-router.post('/getalluserorders', verifyTokenmiddleware.verifyToken,useController.getAllUserOrders)
-
+router.get('/getalluserorders', verifyTokenmiddleware.verifyToken,useController.getAllUserOrders)
 module.exports = router
