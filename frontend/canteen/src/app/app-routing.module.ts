04db606003be2e1ef7facec25b7153638b12a573
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-03-05 23:22:08 +0530

    small change

--- frontend/canteen/src/app/app-routing.module.ts
+++ frontend/canteen/src/app/app-routing.module.ts
@@ -74,6 +74,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes ,  {useHash: true})],
-  exports: [RouterModule] 
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
